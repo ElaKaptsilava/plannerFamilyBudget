45be6f5b59deb837faad39fd512f9a0ed77f0dac
Lizaveta Kaptsilava <126101406+ElaKaptsilava@users.noreply.github.com>
2024-05-10 20:21:42 +0200

    feat(core): set LOGIN_REDIRECT_URL

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -15,32 +15,6 @@ class CustomLoginView(LoginView):
     template_name = "accounts/login.html"
     success_url = reverse_lazy("home")
 
-    # def get_form_kwargs(self):
-    #     """
-    #     Returns the keyword arguments for instantiating the form.
-    #     """
-    #     kwargs = super().get_form_kwargs()
-    #     kwargs.pop('request', None)
-    #     return kwargs
-    #
-    # def post(self, request, *args, **kwargs):
-    #     form = self.form_class(request.POST)
-    #     print(form.errors)
-    #     if form.is_valid():
-    #         try:
-    #             email = form.cleaned_data.get('email')
-    #             password = form.cleaned_data.get('password')
-    #             if email and password:
-    #                 user = CustomUser.objects.get(email=email)
-    #                 if user.check_password(password):
-    #                     user.is_active = True
-    #                     user.save()
-    #                     login(request, user)
-    #                     return redirect(self.success_url)
-    #         except CustomUser.DoesNotExist:
-    #             return self.form_invalid(form)
-    #     return render(request, self.template_name, context={'form': self.form_class})
-
 
 def register_view(request):
     if request.method == "POST":
