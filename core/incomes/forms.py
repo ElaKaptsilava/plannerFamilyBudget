4ded0a5f3cf2466c5f2c852a1bf286f7484a0ecb
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-05-28 18:31:20 +0200

    style(incomes): forms

--- core/incomes/forms.py
+++ core/incomes/forms.py
@@ -6,3 +6,23 @@ class IncomeForm(forms.ModelForm):
     class Meta:
         model = Income
         fields = ["source", "category", "amount", "date"]
+        widgets = {
+            "source": forms.TextInput(
+                attrs={"class": "form-control form-control-user text-secondary"}
+            ),
+            "category": forms.Select(
+                attrs={"class": "form-control form-control-user text-secondary"}
+            ),
+            "amount": forms.NumberInput(
+                attrs={
+                    "class": "form-control form-control-user text-secondary",
+                    "step": "0,01",
+                }
+            ),
+            "date": forms.DateInput(
+                attrs={
+                    "class": "form-control form-control-user text-secondary",
+                    "type": "date",
+                }
+            ),
+        }
