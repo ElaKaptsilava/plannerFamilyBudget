e8ec7cb8b3bb5a33e043684e51d9e1e51c2f74ad
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-05-28 18:23:47 +0200

    style(expenses): forms

--- core/expenses/forms.py
+++ core/expenses/forms.py
@@ -7,3 +7,20 @@ class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ["category", "amount", "datetime"]
+        widgets = {
+            "category": forms.Select(
+                attrs={"class": "form-control form-control-user text-secondary"}
+            ),
+            "amount": forms.NumberInput(
+                attrs={
+                    "class": "form-control form-control-user text-secondary",
+                    "step": "0,01",
+                }
+            ),
+            "datetime": forms.DateInput(
+                attrs={
+                    "class": "form-control form-control-user text-secondary",
+                    "type": "date",
+                }
+            ),
+        }
