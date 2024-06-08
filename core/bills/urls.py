c9bc5451e75bea56b85aaed07206db68aa9b97ea
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-06-08 10:07:02 +0200

    styles(bills): create bill

--- core/bills/urls.py
+++ core/bills/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 app_name = "bills"
 
-urlpatterns = [path("list/", views.BillFormView.as_view(), name="bills-list")]
+urlpatterns = [
+    path("list/", views.BillFormView.as_view(), name="bills-list"),
+]
