77dea42ddab17291a1f8828f796542b847e0f245
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-06-10 12:57:13 +0200

    refactor(budgets_manager): rename form name

--- core/budgets_manager/forms.py
+++ core/budgets_manager/forms.py
@@ -3,17 +3,7 @@ from django import forms
 from .models import BudgetManager
 
 
-class BudgetForm(forms.ModelForm):
+class BudgetManagerForm(forms.ModelForm):
     class Meta:
         model = BudgetManager
         fields = ["savings_percentage", "needs_percentage", "wants_percentage"]
-
-    # def __init__(self, *args, **kwargs):
-    #     user = kwargs.pop("user")
-    #     super().__init__(*args, **kwargs)
-    #     self.fields["category_expense"].queryset = RunningCostCategory.objects.filter(
-    #         user=user
-    #     )
-    #     self.fields["category_running_cost"].queryset = ExpenseCategory.objects.filter(
-    #         user=user
-    #     )
