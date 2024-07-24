8c7df3b0443cda6c2a74527048eafa896c8cf6b5
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-07-24 17:16:12 +0200

    refactor(budgets_manager): reduse querysets to db

--- core/budgets_manager/views/budget/list.py
+++ core/budgets_manager/views/budget/list.py
@@ -10,16 +10,18 @@ class BudgetManagerListView(LoginRequiredMixin, ListView):
     template_name = "budgets_manager/budget/description.html"
 
     def get_queryset(self):
-        return BudgetManager.objects.get(user=self.request.user)
+        return BudgetManager.objects.select_related("user").get(user=self.request.user)
 
     def get_context_data(self, **kwargs):
-        kwargs["needs"] = get_object_or_404(
-            NeedsManager.objects.select_related("user"), user=self.request.user
+        user = self.request.user
+
+        needs = get_object_or_404(
+            NeedsManager.objects.select_related("user"), user=user
         )
-        kwargs["wants"] = get_object_or_404(
-            WantsManager.objects.select_related("user"), user=self.request.user
+        wants = get_object_or_404(
+            WantsManager.objects.select_related("user"), user=user
         )
-        kwargs["savings"] = get_object_or_404(
-            Saving.objects.select_related("user"), user=self.request.user
-        )
+        savings = get_object_or_404(Saving.objects.select_related("user"), user=user)
+
+        kwargs.update({"needs": needs, "wants": wants, "savings": savings})
         return super().get_context_data(**kwargs)
