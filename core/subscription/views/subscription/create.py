814854cad0ce544051aa49e8e5bdc75bb515c88b
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-08-05 11:37:09 +0200

    feat(subscription): implement payment processing with Stripe
    Added , , and  to handle payment processing using Stripe. These views manage the payment workflow, including displaying payment forms, processing Stripe charges, and handling payment success and failure scenarios.

--- core/subscription/views/subscription/create.py
+++ core/subscription/views/subscription/create.py
@@ -6,14 +6,20 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from subscription.forms import SubscriptionForm
-from subscription.models import Subscription
+from subscription.models import Status, Subscription
 
 
 class CreateSubscriptionView(LoginRequiredMixin, CreateView):
     model = Subscription
     form_class = SubscriptionForm
-    success_url = reverse_lazy("home")
     template_name = "subscription/create_subscription.html"
+
+    def get_success_url(self):
+        if self.object.payment.status != Status.COMPLETED:
+            return reverse_lazy(
+                "payment", kwargs={"payment_id": self.object.payment.pk}
+            )
+        return reverse_lazy("home")
 
     def get_context_data(self, **kwargs: typing.Any) -> dict:
         context = super().get_context_data(**kwargs)
