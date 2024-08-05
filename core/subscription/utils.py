0b701765813316dd6151149cb66b6362c4735dd6
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-08-05 11:35:02 +0200

    feat(subscription): add function to check user subscription status

--- core/subscription/utils.py
+++ core/subscription/utils.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse
 from django.urls import reverse_lazy
+from django.utils import timezone
 
 
-def check_is_user_has_subscription(request) -> HttpResponse | None:
+def check_is_user_has_subscription(request) -> str | None:
     subscription_url = reverse_lazy("subscription:list-create")
-    if not hasattr(request.user, "subscription") and request.path != subscription_url:
+    today = timezone.now().date()
+    subscription_exist = request.user.subscription_set.filter(
+        end_date__gte=today, start_date__lte=today
+    ).exists()
+    if request.path != subscription_url and not subscription_exist:
         return subscription_url
