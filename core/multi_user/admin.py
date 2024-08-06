0d42f9c5aee19fb224e58417df58923ec923457d
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-08-06 14:24:40 +0200

    feat(multi_user): register models on admin site

--- core/multi_user/admin.py
+++ core/multi_user/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from multi_user.models import Invitation
+from multi_user.models import Collaboration, CollaborationUser, Invitation
 
 
 @admin.register(Invitation)
 class InvitationAdmin(admin.ModelAdmin):
-    list_display = ["budget", "email", "token"]
-    search_fields = ["budget", "email"]
+    list_display = ["email", "token"]
+    search_fields = ["email"]
+
+
+@admin.register(Collaboration)
+class CollaborationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CollaborationUser)
+class CollaborationUserAdmin(admin.ModelAdmin):
+    list_display = ["user", "collaboration"]
