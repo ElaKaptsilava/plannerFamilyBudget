3c1b1a5262dac722b6f4b012cd2469b2f18277e8
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-09-19 14:57:31 +0200

    refactore(accoutnts): correct getting object in HomeView

--- core/accounts/views/profile/home.py
+++ core/accounts/views/profile/home.py
@@ -1,10 +1,8 @@
 from accounts.models import CustomUser
 from budgets_manager.models import BudgetManager, NeedsManager, WantsManager
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
-from multi_user.models import FamilyBudget
 from targets.forms import (
     SavingNegativeContributionsForm,
     SavingPositiveContributionsForm,
@@ -17,19 +15,18 @@ class HomeView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         user = self.request.user
+        budget = self.get_budget_object(user)
 
         kwargs.update(
             {
-                "budget": self.get_budget_object(user),
-                "family_budget": self.get_family_budgets_object(user),
-                "needs": self.get_needs_manager_object_or_404(user),
-                "wants": self.get_wants_manager_object_or_404(user),
+                "budget": budget,
+                "needs": self.get_needs_manager_object(budget),
+                "wants": self.get_wants_manager_object(budget),
                 "savings": self.get_savings_object_or_404(user),
                 "positive_form": SavingPositiveContributionsForm(),
                 "negative_form": SavingNegativeContributionsForm(),
             }
         )
-        print(self.get_family_budgets_object(user), "family")
         return super().get_context_data(**kwargs)
 
     @staticmethod
@@ -37,21 +34,13 @@ class HomeView(LoginRequiredMixin, TemplateView):
         return user.set_budget.budget
 
     @staticmethod
-    def get_needs_manager_object_or_404(user: CustomUser) -> NeedsManager:
-        return get_object_or_404(NeedsManager.objects.select_related("user"), user=user)
+    def get_needs_manager_object(budget: BudgetManager) -> NeedsManager:
+        return get_object_or_404(NeedsManager, pk=budget.pk)
 
     @staticmethod
-    def get_wants_manager_object_or_404(user: CustomUser) -> WantsManager:
-        return get_object_or_404(WantsManager.objects.select_related("user"), user=user)
+    def get_wants_manager_object(budget: BudgetManager) -> WantsManager:
+        return get_object_or_404(WantsManager, pk=budget.pk)
 
     @staticmethod
     def get_savings_object_or_404(user: CustomUser) -> Saving:
         return get_object_or_404(Saving.objects.select_related("user"), user=user)
-
-    @staticmethod
-    def get_family_budgets_object(user: CustomUser) -> QuerySet[FamilyBudget]:
-        try:
-            family_budget = FamilyBudget.objects.get(budget_manager=user.budgetmanager)
-        except Exception:
-            return None
-        return family_budget
