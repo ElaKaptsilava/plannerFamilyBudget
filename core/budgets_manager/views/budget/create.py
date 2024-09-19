4d9deabddda7ea01241efa9c0d1a48f14776a0ad
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-09-19 15:03:39 +0200

    refactore(budgets_manager): add SuccessMessageMixin to views

--- core/budgets_manager/views/budget/create.py
+++ core/budgets_manager/views/budget/create.py
@@ -1,17 +1,19 @@
 from budgets_manager.forms.limit import BudgetManagerForm
 from budgets_manager.models import BudgetManager
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
-class BudgetManagerCreateView(LoginRequiredMixin, CreateView):
+class BudgetManagerCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     form_class = BudgetManagerForm
     template_name = "budgets_manager/budget/budget-create.html"
     context_object_name = "budget"
     model = BudgetManager
     success_url = reverse_lazy("subscription:list-create")
+    success_message = "Budget Manager has been created"
 
     def form_valid(self, form) -> HttpResponse:
         form.instance.user = self.request.user
--- core/budgets_manager/views/budget/update.py
+++ core/budgets_manager/views/budget/update.py
@@ -2,16 +2,18 @@ from budgets_manager.forms.limit import BudgetManagerForm
 from budgets_manager.models import BudgetManager
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 
 
-class UpdateBudgetView(LoginRequiredMixin, UpdateView):
+class UpdateBudgetView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     form_class = BudgetManagerForm
     template_name = "budgets_manager/budget/budget.html"
     context_object_name = "budget"
     model = BudgetManager
     success_url = reverse_lazy("home")
+    success_message = "Budget updated successfully!"
 
     def form_invalid(self, form):
         messages.info(self.request, "Check budget data!")
