550615fd585b6d6ff1cf1a7c389afc71377ef691
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-09-19 14:41:22 +0200

    refactore(budget_manager): rename methods in Limit model

--- core/budgets_manager/models/limit.py
+++ core/budgets_manager/models/limit.py
@@ -45,6 +45,37 @@ class LimitManager(models.Model):
         help_text="Amount allocated to this budget entry.",
     )
 
+    def __str__(self) -> str:
+        return f"{self.budget_manager}'s Limit - {self.type} - {self.amount}"
+
+    def __repr__(self) -> str:
+        return f"Limit(user={self.budget_manager!r}, type={self.type!r}, amount={self.amount!r})"
+
+    def save(self, *args, **kwargs) -> None:
+        self.__validate_budget_type()
+        self.__validate_amount()
+        super().save(*args, **kwargs)
+
+    def __validate_budget_type(self):
+        if self.type == Type.NEEDS and self.target:
+            raise ValidationError(
+                "For 'needs' type, either category expense or category running cost must be set."
+            )
+        elif self.type == Type.WANTS and self.category_running_cost:
+            raise ValidationError(
+                "For 'wants' type, target or category expense must be set."
+            )
+
+    def __validate_amount(self):
+        limits = {
+            Type.NEEDS: NeedsManager.objects.get(pk=self.budget_manager.pk).get_limit,
+            Type.WANTS: WantsManager.objects.get(pk=self.budget_manager.pk).get_limit,
+        }
+        if self.amount > limits.get(self.type):
+            raise ValidationError(
+                f"The amount should be less than the {int(limits.get(self.type))}..."
+            )
+
     @property
     def within_limit(self) -> bool:
         return self.calculate_total_spent() < float(self.amount)
@@ -69,13 +100,13 @@ class LimitManager(models.Model):
 
     def calculate_total_spent(self) -> float:
         if self.category_expense:
-            return self._calculate_total_spent_for_category_expense()
+            return self.total_spent_on_category_expense()
         elif self.type == Type.NEEDS and self.category_running_cost:
-            return self._calculate_total_spent_for_category_running_cost()
+            return self.total_spent_on_category_running_cost()
         elif self.type == Type.WANTS and self.target:
-            return self._calculate_wants_spent()
+            return self.total_wants_spent()
 
-    def _calculate_total_spent_for_category_running_cost(self) -> float:
+    def total_spent_on_category_running_cost(self) -> float:
         current_costs = self.category_running_cost.running_costs.filter(
             next_payment_date__range=self.budget_manager.get_current_month_range(),
         )
@@ -86,14 +117,14 @@ class LimitManager(models.Model):
         )
         return total_spent or 0.0
 
-    def _calculate_total_spent_for_category_expense(self) -> float:
-        filtered_expenses = self.category_expense.expense_set.filter(
-            datetime__range=self.budget_manager.get_current_month_range(),
+    def total_spent_on_category_expense(self) -> float:
+        expenses = self.budget_manager.get_monthly_expenses().filter(
+            category=self.category_expense
         )
-        total = filtered_expenses.aggregate(total=models.Sum("amount"))["total"] or 0.0
+        total = expenses.aggregate(total=models.Sum("amount"))["total"] or 0.0
         return total
 
-    def _calculate_wants_spent(self) -> float:
+    def total_wants_spent(self) -> float:
         filtered_target_contributions = self.target.targetcontribution_set.filter(
             date__range=self.budget_manager.get_current_month_range(),
         )
@@ -102,40 +133,3 @@ class LimitManager(models.Model):
             or 0.0
         )
         return total
-
-    def save(self, *args, **kwargs) -> None:
-        self.__validate_budget_type()
-        self.__validate_amount()
-        super().save(*args, **kwargs)
-
-    def __validate_budget_type(self):
-        if self.type == Type.NEEDS and self.target:
-            raise ValidationError(
-                "For 'needs' type, either category expense or category running cost must be set."
-            )
-        elif self.type == Type.WANTS and self.category_running_cost:
-            raise ValidationError(
-                "For 'wants' type, target or category expense must be set."
-            )
-
-    def __validate_amount(self):
-        limits = {
-            Type.NEEDS: NeedsManager.objects.get(
-                user=self.budget_manager.user
-            ).get_limit,
-            Type.WANTS: WantsManager.objects.get(
-                user=self.budget_manager.user
-            ).get_limit,
-        }
-        if self.amount > limits.get(self.type):
-            raise ValidationError(
-                f"The amount should be less than the {int(limits.get(self.type))}..."
-            )
-
-    def __str__(self) -> str:
-        return (
-            f"{self.budget_manager.user.username}'s Limit - {self.type} - {self.amount}"
-        )
-
-    def __repr__(self) -> str:
-        return f"Limit(user={self.budget_manager.user.username!r}, type={self.type!r}, amount={self.amount!r})"
