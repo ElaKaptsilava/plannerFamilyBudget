ee2dadd2fd72361117a0ead2ccba847951635290
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-10-09 15:34:56 +0200

    feat(budget_manager): get success url

--- core/budgets_manager/views/budget/create.py
+++ core/budgets_manager/views/budget/create.py
@@ -1,5 +1,6 @@
 from budgets_manager.forms.limit import BudgetManagerForm
 from budgets_manager.models import BudgetManager
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
@@ -12,11 +13,26 @@ class BudgetManagerCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateVie
     template_name = "budgets_manager/budget/budget-create.html"
     context_object_name = "budget"
     model = BudgetManager
-    success_url = reverse_lazy("subscription:list-create")
     success_message = "Budget Manager has been created"
+
+    def get_success_url(self):
+        subscriptions = self.request.user.subscription_set
+        if subscriptions.exists():
+            last_subscription = subscriptions.last()
+            if last_subscription.is_active:
+                return reverse_lazy("home")
+            else:
+                return reverse_lazy(
+                    "payment", kwargs={"payment_id": last_subscription.pk}
+                )
+        return reverse_lazy("subscription:list-create")
 
     def form_valid(self, form) -> HttpResponse:
         form.instance.user = self.request.user
         response = super().form_valid(form)
         form.instance.participants.add(self.request.user)
         return response
+
+    def form_invalid(self, form) -> HttpResponse:
+        messages.error(self.request, "Please try again.")
+        return super().form_invalid(form)
