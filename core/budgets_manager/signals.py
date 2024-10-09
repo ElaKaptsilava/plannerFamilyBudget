36dbb9e6ed459a2ca6aa58a76af01677e8b03f1d
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-10-09 15:27:46 +0200

    feat(budget_managet): set budget into create_monthly_income singals

--- core/budgets_manager/signals.py
+++ core/budgets_manager/signals.py
@@ -1,4 +1,4 @@
-from budgets_manager.models import BudgetManager, MonthlyIncomes
+from budgets_manager.models import BudgetManager, MonthlyIncomes, SetBudget
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
@@ -13,3 +13,5 @@ def create_monthly_income(sender, instance, created, **kwargs):
             budget=instance, year=year, month=month
         )
         monthly_income.save()
+        set_budget = SetBudget.objects.create(user=instance.user, budget=instance)
+        set_budget.save()
