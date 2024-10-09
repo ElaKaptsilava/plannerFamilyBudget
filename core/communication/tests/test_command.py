c465e822844c58ce13ced8da2129b763453d6768
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-10-09 17:18:20 +0200

    test(communication): test commen that create messege to all users with OpenAI

--- core/communication/tests/test_command.py
+++ core/communication/tests/test_command.py
@@ -1,31 +1,42 @@
+from io import StringIO
+from unittest.mock import patch
+
 from accounts.tests import CustomUserFactory
 from budgets_manager.tests.factories.budget_manager_factory import BudgetManagerFactory
+from communication.management.commands.open_ai_message import Command
+from communication.models import Message
+from django.core.management import call_command
 from django.test import TestCase
+from django.utils import timezone
 from expenses.tests.factories import ExpenseCategoryFactory, ExpenseFactory
-from expenses.types import Type
 from incomes.tests.factories import IncomeFactory
 from runningCosts.tests.factories import RunningCostCategoryFactory, RunningCostFactory
+from subscription.models import Status
+from subscription.tests.factories import PlanFactory, SubscriptionFactory
 
 
 class CommandTestCase(TestCase):
     def setUp(self):
         self.user = CustomUserFactory.create()
         self.budget = BudgetManagerFactory.create(user=self.user)
-        self.incomes = IncomeFactory.create_batch(user=self.user, size=3)
-        self.expense_category1 = ExpenseCategoryFactory.create(user=self.user)
-        self.expense_category2 = ExpenseCategoryFactory.create(
-            user=self.user, type=Type.WANTS
+
+        self.plan = PlanFactory(price=100)
+        self.subscription = SubscriptionFactory.create(user=self.user, plan=self.plan)
+        self.subscription.payment.status = Status.COMPLETED
+        self.subscription.save()
+
+        self.incomes = IncomeFactory.create_batch(
+            user=self.user, budget=self.budget, size=3
         )
+        self.expense_category = ExpenseCategoryFactory.create(user=self.user)
         self.expense = ExpenseFactory.create(
-            user=self.user, category=self.expense_category1
-        )
-        self.expense2 = ExpenseFactory.create(
-            user=self.user, category=self.expense_category2
+            user=self.user, category=self.expense_category, budget=self.budget
         )
         self.category_cost = RunningCostCategoryFactory.create(user=self.user)
         self.running_cost = RunningCostFactory.create(
             user=self.user, category=self.category_cost
         )
+
         self.success_message = "Successfully created alert messages with openai."
 
     @staticmethod
@@ -33,32 +44,34 @@ class CommandTestCase(TestCase):
         with open("communication/tests/open_ai_output.txt") as response:
             return response.read()
 
-    # @patch(
-    #     "communication.management.commands.open_ai_message.Command.get_openai_response"
-    # )
-    # def test_command_output(self, mock_get_openai_response):
-    #     mock_get_openai_response.return_value = self.mock_open_ai_response()
-    #     out = StringIO()
-    #
-    #     self.client.force_login(self.user)
-    #
-    #     with patch("sys.stdout", new=out):
-    #         call_command("open_ai_message")
-    #
-    #     captured_output = out.getvalue().strip()
-    #     self.assertEqual(captured_output, self.success_message)
+    @patch(
+        "communication.management.commands.open_ai_message.Command.get_openai_response"
+    )
+    def test_command_output(self, mock_get_openai_response):
+        mock_get_openai_response.return_value = self.mock_open_ai_response()
+        out = StringIO()
+        self.client.force_login(self.user)
+        with patch("sys.stdout", new=out):
+            call_command("open_ai_message")
+        #
+        captured_output = out.getvalue().strip()
+        print(captured_output)
+        self.assertEqual(captured_output, self.success_message)
 
-    # @patch(
-    #     "communication.management.commands.open_ai_message.Command.get_openai_response"
-    # )
-    # def test_create_budget_message(self, mock_get_openai_response):
-    #     mock_get_openai_response.return_value = self.mock_open_ai_response()
-    #     mock_now = timezone.now()
-    #
-    #     with patch("django.utils.timezone.now", return_value=mock_now):
-    #         command = Command()
-    #         command.create_budget_message()
-    #
-    #     messages = Message.objects.all()
-    #     expected_title = f"Budget analiz {mock_now.strftime('%B')} {mock_now.year}"
-    #     self.assertTrue(all(message.title == expected_title for message in messages))
+    @patch(
+        "communication.management.commands.open_ai_message.Command.get_openai_response"
+    )
+    def test_create_budget_message(self, mock_get_openai_response):
+        mock_get_openai_response.return_value = self.mock_open_ai_response()
+        mock_now = timezone.now()
+
+        with patch("django.utils.timezone.now", return_value=mock_now):
+            command = Command()
+            command.create_budget_message()
+
+        message = Message.objects.first()
+
+        date_str = f"{message.created_at.strftime('%d')} {message.created_at.strftime('%B')} {message.created_at.strftime('%Y')}"
+        expected_title = f"Budget analiz {date_str} for {message.user.username}"
+
+        self.assertTrue(message.title == expected_title)
